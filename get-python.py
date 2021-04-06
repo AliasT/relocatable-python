f696795d3f3f2aff45c4579460b53c33c5aca532
ryan <chai_xb@163.com>
2021-04-06 16:24:23 +0800

    fix

--- get-python.py
+++ get-python.py
@@ -9,10 +9,10 @@ print(sys.executable)
 # For windows
 executable_path = os.path.dirname(sys.executable)
 requirements_path = os.path.join(os.getcwd(), "requirements.txt")
-cmd = [executable_path, "-s", "-m", "pip", "install", "-r", requirements_path]
+cmd = [sys.executable, "-s", "-m", "pip", "install", "-r", requirements_path]
 
 print("Installing modules from %s..." % requirements_path)
 
 subprocess.check_call(cmd)
 
-shutil.make_archive("python3win", 'zip', executable_path)
+shutil.make_archive("python3Win", 'zip', executable_path)
