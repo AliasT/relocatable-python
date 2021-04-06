94e72c0d7006eafe4425c64f807f8ba23fc2df39
ryan <chai_xb@163.com>
2021-04-06 15:20:48 +0800

    include libs

--- get-python.py
+++ get-python.py
@@ -1,9 +1,18 @@
 import sys
 import os
+import subprocess
 
 import shutil
+print(os.getcwd())
+print(sys.executable)
 
-
+# For windows
 executable_path = os.path.dirname(sys.executable)
+requirements_path = os.path.join(os.getcwd(), "requirements.txt")
+cmd = [executable_path, "-s", "-m", "pip", "install", "-r", requirements_path]
+
+print("Installing modules from %s..." % requirements_path)
+
+subprocess.check_call(cmd)
 
 shutil.make_archive("python3win", 'zip', executable_path)
